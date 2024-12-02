016073a0fc90e9ffde08d17f849aa1711396cc1f
Bálint Faragó <eaglelor@gmail.com>
2024-12-02 05:47:01 +0100

    Assume length, don't create slices

--- src/day1.rs
+++ src/day1.rs
@@ -1,31 +1,26 @@
 use memchr::memchr;
 
 #[inline]
-fn parse_digits(input: &[u8]) -> u64 {
+fn parse_digits(input: &[u8], idx: usize, digit_len: usize) -> u32 {
     let mut result = 0;
-    for byte in input {
-        result = result * 10 + (byte - b'0') as u64;
+    for i in idx..idx + digit_len {
+        result = result * 10 + (unsafe { input.get_unchecked(i) } - b'0') as u32;
     }
     result
 }
 
-pub fn part1(input: &str) -> u64 {
-    let input = input.as_bytes();
+fn part1_flexible(input: &[u8], digit_len: usize) -> u64 {
     let mut left = Vec::<u32>::with_capacity(1000);
     let mut right = Vec::<u32>::with_capacity(1000);
     let mut idx = 0;
-    while idx < input.len() {
-        let space_idx = unsafe { memchr(b' ', &input[idx..]).unwrap_unchecked() };
-        let l = parse_digits(&input[idx..idx + space_idx]) as u32;
+    while idx < input.len() {        
+        let l = parse_digits(input, idx, digit_len);
         left.push(l);
-        idx = idx + space_idx + 1;
-        while unsafe { *input.get_unchecked(idx) } == b' ' {
-            idx += 1;
-        }
-        let newline_idx = unsafe { memchr(b'\n', &input[idx..]).unwrap_unchecked() };
-        let r =parse_digits(&input[idx..idx + newline_idx]) as u32;
+        idx = idx + digit_len + 3;
+
+        let r =parse_digits(input, idx, digit_len);
         right.push(r);
-        idx = idx + newline_idx + 1;        
+        idx = idx + digit_len + 1;
     }
 
     radsort::sort(&mut left);
@@ -36,29 +31,95 @@ pub fn part1(input: &str) -> u64 {
         .sum::<u64>()
 }
 
-pub fn part2(input: &str) -> u64 {
+#[inline]
+fn parse_digits_5(input: &[u8], idx: usize) -> u32 {
+    let mut result = 0;
+    for i in idx..idx + 5 {
+        result = result * 10 + (unsafe { input.get_unchecked(i) } - b'0') as u32;
+    }
+    result
+}
+
+fn part1_assume_length(input: &[u8]) -> u64 {
+    let mut left = Vec::<u32>::with_capacity(1000);
+    let mut right = Vec::<u32>::with_capacity(1000);
+    let mut idx = 0;
+    while idx < input.len() {
+        let l = parse_digits_5(input, idx);
+        left.push(l);
+        idx += 5 + 3;
+
+        let r =parse_digits_5(input, idx);
+        right.push(r);
+        idx += 5 + 1;
+    }
+
+    radsort::sort(&mut left);
+    radsort::sort(&mut right);
+    left.into_iter()
+        .zip(right)
+        .map(|(l, r)| l.abs_diff(r) as u64)
+        .sum::<u64>()
+}
+
+pub fn part1(input: &str) -> u64 {
     let input = input.as_bytes();
+    let digit_len = unsafe { memchr(b' ', input).unwrap_unchecked() };
+    if digit_len == 5 {
+        part1_assume_length(input)
+    } else {
+        part1_flexible(input, digit_len)
+    }
+}
+
+fn part2_flexible(input: &[u8], digit_len: usize) -> u64 {
     let mut left = Vec::<u32>::with_capacity(1000);
     let mut counts: Vec<u16> = vec![0; 100000];
     let mut idx = 0;
     while idx < input.len() {
-        let space_idx = unsafe { memchr(b' ', &input[idx..]).unwrap_unchecked() };
-        let l = parse_digits(&input[idx..idx + space_idx]) as u32;
+        let l = parse_digits(input, idx, digit_len);
         left.push(l);
-        idx = idx + space_idx + 1;
-        while unsafe { *input.get_unchecked(idx) } == b' ' {
-            idx += 1;
-        }
-        let newline_idx = unsafe { memchr(b'\n', &input[idx..]).unwrap_unchecked() };
-        let r =parse_digits(&input[idx..idx + newline_idx]);
+        idx = idx + digit_len + 3;
+
+        let r =parse_digits(input, idx, digit_len);
         unsafe { *counts.get_unchecked_mut(r as usize) += 1; }
-        idx = idx + newline_idx + 1;        
+        idx = idx + digit_len + 1;
     }
 
     left.into_iter()
         .map(|l| l as u64 * (*unsafe {
             counts.get_unchecked(l as usize)
         } as u64)).sum()
+}
+
+fn part2_assume_length(input: &[u8]) -> u64 {
+    let mut left = Vec::<u32>::with_capacity(1000);
+    let mut counts: Vec<u16> = vec![0; 100000];
+    let mut idx = 0;
+    while idx < input.len() {
+        let l = parse_digits_5(input, idx);
+        left.push(l);
+        idx = idx + 5 + 3;
+
+        let r =parse_digits_5(input, idx);
+        unsafe { *counts.get_unchecked_mut(r as usize) += 1; }
+        idx = idx + 5 + 1;
+    }
+
+    left.into_iter()
+        .map(|l| l as u64 * (*unsafe {
+            counts.get_unchecked(l as usize)
+        } as u64)).sum()
+}
+
+pub fn part2(input: &str) -> u64 {
+    let input = input.as_bytes();
+    let digit_len = unsafe { memchr(b' ', input).unwrap_unchecked() };
+    if digit_len == 5 {
+        part2_assume_length(input)
+    } else {
+        part2_flexible(input, digit_len)
+    }
 }
 
 #[cfg(test)]
