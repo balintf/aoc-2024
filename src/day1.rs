8a417cc61945fa07d96f445c7a346f85e7004715
Bálint Faragó <eaglelor@gmail.com>
2024-12-02 05:10:30 +0100

    Use array, use radix sort

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,4 @@
-use std::collections::HashMap;
 use memchr::memchr;
-use rustc_hash::FxBuildHasher;
 
 #[inline]
 fn parse_digits(input: &[u8]) -> u64 {
@@ -13,39 +11,39 @@ fn parse_digits(input: &[u8]) -> u64 {
 
 pub fn part1(input: &str) -> u64 {
     let input = input.as_bytes();
-    let mut left = Vec::<u64>::with_capacity(1000);
-    let mut right = Vec::<u64>::with_capacity(1000);
+    let mut left = Vec::<u32>::with_capacity(1000);
+    let mut right = Vec::<u32>::with_capacity(1000);
     let mut idx = 0;
     while idx < input.len() {
         let space_idx = unsafe { memchr(b' ', &input[idx..]).unwrap_unchecked() };
-        let l = parse_digits(&input[idx..idx + space_idx]);
+        let l = parse_digits(&input[idx..idx + space_idx]) as u32;
         left.push(l);
         idx = idx + space_idx + 1;
         while unsafe { *input.get_unchecked(idx) } == b' ' {
             idx += 1;
         }
         let newline_idx = unsafe { memchr(b'\n', &input[idx..]).unwrap_unchecked() };
-        let r =parse_digits(&input[idx..idx + newline_idx]);
+        let r =parse_digits(&input[idx..idx + newline_idx]) as u32;
         right.push(r);
         idx = idx + newline_idx + 1;        
     }
 
-    left.sort_unstable();
-    right.sort_unstable();
+    radsort::sort(&mut left);
+    radsort::sort(&mut right);
     left.into_iter()
         .zip(right)
-        .map(|(l, r)| l.abs_diff(r))
-        .sum()
+        .map(|(l, r)| l.abs_diff(r) as u64)
+        .sum::<u64>()
 }
 
 pub fn part2(input: &str) -> u64 {
     let input = input.as_bytes();
-    let mut left = Vec::<u64>::with_capacity(1000);
-    let mut right = HashMap::<u64, u32, FxBuildHasher>::with_capacity_and_hasher(1000, FxBuildHasher::default());
+    let mut left = Vec::<u32>::with_capacity(1000);
+    let mut counts: Vec<u16> = vec![0; 100000];
     let mut idx = 0;
     while idx < input.len() {
         let space_idx = unsafe { memchr(b' ', &input[idx..]).unwrap_unchecked() };
-        let l = parse_digits(&input[idx..idx + space_idx]);
+        let l = parse_digits(&input[idx..idx + space_idx]) as u32;
         left.push(l);
         idx = idx + space_idx + 1;
         while unsafe { *input.get_unchecked(idx) } == b' ' {
@@ -53,13 +51,14 @@ pub fn part2(input: &str) -> u64 {
         }
         let newline_idx = unsafe { memchr(b'\n', &input[idx..]).unwrap_unchecked() };
         let r =parse_digits(&input[idx..idx + newline_idx]);
-        right.entry(r).and_modify(|e| *e += 1).or_insert(1);
+        unsafe { *counts.get_unchecked_mut(r as usize) += 1; }
         idx = idx + newline_idx + 1;        
     }
 
     left.into_iter()
-        .map(|l| right.get(&l).copied().unwrap_or_default() as u64 * l)
-        .sum()
+        .map(|l| l as u64 * (*unsafe {
+            counts.get_unchecked(l as usize)
+        } as u64)).sum()
 }
 
 #[cfg(test)]
